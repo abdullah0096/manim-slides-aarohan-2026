dc8b230cf05689c10364801a6933757ba0f1de75
A_Abdullah <abdullah0096@yahoo.com>
2026-02-16 19:30:04 +0530

    Added requirements.txt

---
+++ utils.py
@@ -0,0 +1,9 @@
+from manim import *
+from manim_slides import Slide
+
+# DOTCOLOR = WHITE
+# def plotDotTL(s):
+#     dot = Dot(color=DOTCOLOR).to_corner(UL)
+
+#     s.play(FadeIn(dot), run_time=0.1)
+#     s.play(FadeOut(dot), run_time=0.1)
